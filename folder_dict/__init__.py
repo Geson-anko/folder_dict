0b61481c819367e4e781dc4dcf3afb64fa5365dc
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-04-14 11:02:56 +0900

    version 0.1.2

--- folder_dict/__init__.py
+++ folder_dict/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from path_dict import PathDict
 from typing import *
--- tests/test_folder_dict.py
+++ tests/test_folder_dict.py
@@ -13,7 +13,7 @@ user =  {
 
 
 def test_version():
-    assert __version__ == '0.1.1'
+    assert __version__ == '0.1.2'
 
 def test_constructor():
     """testing constructor"""
